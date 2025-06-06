160a95d2e403260c5221c5aee2ab730944531a41
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2025-06-06 22:58:02 +0100

    Patched Pathfinding to only allow navigation if we're the host

---
+++ ClassLibrary1/Patches/PathfindingPatch.cs
@@ -0,0 +1,24 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using ONI_MP.Networking;
+using ONI_MP.DebugTools;
+using STRINGS;
+
+namespace ONI_MP.Patches
+{
+    [HarmonyPatch(typeof(Pathfinding), nameof(Pathfinding.UpdateNavGrids))]
+    public static class Pathfinding_UpdateNavGrids_Patch
+    {
+        public static bool Prefix(ref bool update_all)
+        {
+            if (!MultiplayerSession.IsHost)
+            {
+                //DebugConsole.LogError("We are not the host!");
+                return false; // Skip updating navgrids for clients
+            }
+
+            return true; // Allow host to run original method
+        }
+    }
+
+}

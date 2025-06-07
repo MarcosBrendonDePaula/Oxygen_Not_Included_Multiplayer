000f73eb3e04e9c5d5d3019ca52abfa000ddc491
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2025-06-07 20:17:52 +0100

    Host now replies to PingPackets with PongPackets

--- ClassLibrary1/Networking/Packets/PingPacket.cs
+++ ClassLibrary1/Networking/Packets/PingPacket.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.IO;
+using Steamworks;
+using ONI_MP.Networking.Packets;
+using ONI_MP.Networking.Packets.ONI_MP.Networking.Packets;
 
 namespace ONI_MP.Networking.Packets
 {
     public class PingPacket : IPacket
     {
         public long Timestamp; // in ticks (DateTime.UtcNow.Ticks)
+        public CSteamID SenderID; // The ID of the sender
 
         public PacketType Type => PacketType.Ping;
 
@@ -23,6 +27,11 @@ namespace ONI_MP.Networking.Packets
         {
             // Client sends this to the host, so no local logic needed.
             // Host should respond with a PongPacket in the handler.
+            var packet = new PongPacket
+            {
+                Timestamp = this.Timestamp
+            };
+            PacketSender.SendToPlayer(SenderID, packet);
         }
     }
 }

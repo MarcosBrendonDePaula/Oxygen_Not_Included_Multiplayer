0a827e7c3998d24cdd4b39c65dbdd5c8e4c98144
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2025-06-07 22:59:08 +0100

    Leave lobby if we're in a session when returning to the main menu

---
+++ ClassLibrary1/Patches/PauseScreenPatch.cs
@@ -0,0 +1,25 @@
+﻿using HarmonyLib;
+using JetBrains.Annotations;
+using ONI_MP.DebugTools;
+using ONI_MP.Networking;
+using UnityEngine;
+
+namespace ONI_MP.Patches
+{
+    [HarmonyPatch(typeof(PauseScreen), "OnQuitConfirm")]
+    public static class PauseScreenPatch
+    {
+        [HarmonyPrefix]
+        [UsedImplicitly]
+        public static void OnQuitConfirm_Prefix(bool saveFirst)
+        {
+            if (MultiplayerSession.InSession)
+            {
+                SteamLobby.LeaveLobby();
+                DebugConsole.Log("Left Steam lobby before quitting to main menu.");
+                MultiplayerSession.Clear();
+            }
+
+        }
+    }
+}

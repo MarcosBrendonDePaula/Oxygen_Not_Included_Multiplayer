960001a73e3082d4832a595d956eee0ef900a717
Lyraedan <61066562+Lyraedan@users.noreply.github.com>
2025-12-20 19:47:38 +0000

    Disabled capturing world snapshots when losing connection or disconnecting as it causes crashes

--- ClassLibrary1/Networking/GameClient.cs
+++ ClassLibrary1/Networking/GameClient.cs
@@ -90,7 +90,7 @@ namespace ONI_MP.Networking
 				Connection = null;
 				SetState(ClientState.Disconnected);
 				MultiplayerSession.InSession = false;
-				SaveHelper.CaptureWorldSnapshot();
+				//SaveHelper.CaptureWorldSnapshot();
 			}
 			else
 			{
@@ -270,7 +270,7 @@ namespace ONI_MP.Networking
 		private static IEnumerator ShowMessageAndReturnToTitle()
 		{
 			MultiplayerOverlay.Show("Connection to the host was lost!");
-            SaveHelper.CaptureWorldSnapshot();
+            //SaveHelper.CaptureWorldSnapshot();
             yield return new WaitForSeconds(3f);
             //PauseScreen.TriggerQuitGame(); // Force exit to frontend, getting a crash here
 
